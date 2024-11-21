424a8a92c419e9d9a8bde0a4bffe307d3fa7dd4c
davidrey <david_sardy@hotmail.com>
2024-11-21 18:49:09 +0100

    Exercise 15 feito

--- src/main/java/com/campusdual/classroom/FreshMerchandise.java
+++ src/main/java/com/campusdual/classroom/FreshMerchandise.java
@@ -5,4 +5,42 @@ import java.util.Date;
 
 public class FreshMerchandise extends Merchandise {
 
+    protected Date expirationDate = new Date(123, 4, 4);//modifico a fecha segun os params de date
+    protected SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");//modifico o formato para que mostre dias meses e ano respectivamente
+
+    public FreshMerchandise(String name, String uniqueId, String responsibleId) {
+        super(name, uniqueId, responsibleId);
+    }
+
+    public FreshMerchandise(String name, String uniqueId, String responsibleId, int zone, String area, String shelf, int quantity, Date expirationDate) {
+        super(name, uniqueId, responsibleId, zone, area, shelf, quantity);
+
+    }
+
+    public Date getExpirationDate() {
+        return this.expirationDate;
+    }
+
+    @Override
+    public Object getSpecificData() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Localización: ");
+        builder.append(getLocation());
+        builder.append("\n");
+        builder.append("Caducidade: ");
+        builder.append(sdf.format(this.getExpirationDate()));
+        return builder.toString();
+    }
+
+
+    public void printSpecificData() {
+        System.out.println(getSpecificData());
+
+    }
+
+    public String getFormattedDate(Date expirationDate) {//Este metodo toma o expirationDate e o formatea como lle dixen arriba no sdf
+        return sdf.format(expirationDate);
+    }
+
+
 }
